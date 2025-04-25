a03e28d0a10cd4f82124ddebda7334cb0daeab1c
ksupdev <karoons.updev@gmail.com>
2025-04-25 16:01:20 +0700

    Add grid limit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import sampleData from '@/db/sample-data';
 
 const Homepage = () => {
   return <>
-    <ProductList data={sampleData.products} title='Newest Arrivals' />
+    <ProductList data={sampleData.products} title='Newest Arrivals' limit={2} />
   </>;
 }
 
--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,12 +1,14 @@
 //sfc
-const ProductList = ({ data, title }: { data: any; title?: string }) => {
+const ProductList = ({ data, title, limit }: { data: any; title?: string; limit?: number }) => {
+    const limitedData = limit ? data.slice(0, limit) : data;
+
     return (<div className="my-10">
         <h2 className='h2-bold mb-4'>
             {title}
         </h2>
-        {data.length > 0 ? (
+        {limitedData.length > 0 ? (
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-                {data.map((product: any, index: number) =>
+                {limitedData.map((product: any, index: number) =>
                     (<div key={index}>{product.name}</div>))}
             </div>
         ) : (<div>
